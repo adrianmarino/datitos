b41dacd076067dffcf4f1e6f3cf52aced4f29c9a
adrianmarino <adrianmarino@gmail.com>
2021-08-18 09:51:42 -0300

    feat: callbacks abstraction refactor andd acc metrics

--- lib/model/loss.py
+++ lib/model/loss.py
@@ -3,10 +3,10 @@ from utils import try_gpu
 import torch
 import numpy as np
 
-def softmax_cross_entropy_fn(y_hat, y):
+def softmax_cross_entropy_fn(y_hat, y, reduction='mean'):
     # print(y, max_arg(y))
     y_max_arg = torch.max(y, 1)[1]
-    return F.cross_entropy(y_hat, y_max_arg)
+    return F.cross_entropy(y_hat, y_max_arg, reduction=reduction)
 
 
 def softmax_pred_out(y_hat): return np.argmax(y_hat, axis=1)
